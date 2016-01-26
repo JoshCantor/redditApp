76393fdfb9aea243df3ace62429706d7f57aa6be
JoshCantor <josh.s.cantor@gmail.com>
2016-01-25 16:20:21 -0800

    add votes attribute to each post; user can upvote and downvote

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = angular.module("redditApp", []);
 
 app.controller("AppController", function($scope) {
 	$scope.post = {};
+	$scope.post.votes = 0;
 	$scope.addPost = false;
 	$scope.newPost = function() {
 		$scope.addPost = true;
@@ -15,11 +16,34 @@ app.controller("AppController", function($scope) {
 		$scope.displayInitialPost = true;
 		
 		var postCopy = {};
+		$scope.post.votes = 0;
 		for (key in $scope.post) {
 			postCopy[key] = $scope.post[key];
 		}
 		$scope.currentPosts.push(postCopy);
 	}
+	
+	$scope.upVote = function(thisPost) {
+		thisPost.votes += 1;
+	}
+
+	$scope.downVote = function(thisPost) {
+		thisPost.votes -= 1;
+	}
+
+	$scope.isPositive = function(voteTotal) {
+		return (voteTotal > 0);
+	}
+
+	$scope.isNegative = function(voteTotal) {
+		return (voteTotal < 0);
+	}
+
+	$scope.isZero = function(voteTotal) {
+		return (voteTotal === 0);
+	}
+
+
 
 
 });
